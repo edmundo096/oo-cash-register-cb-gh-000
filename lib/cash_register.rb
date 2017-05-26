49f626226f2df0fdb7e38f58f7be02fac19c1ce6
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-05-26 03:32:30 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+class CashRegister
 
+  attr_accessor :total # NOTE: Should be reader only, but rspec fails if so.
+  attr_reader :discount, :items, :last
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @last = {}
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times { @items << title }
+
+    @last[:title] = title
+    @last[:price] = price
+    @last[:quantity] = quantity
+    nil
+  end
+
+  def apply_discount
+    return "There is no discount to apply." if @discount == 0
+
+    @total -= @total * (@discount / 100.0)
+    @total = @total.to_i
+    "After the discount, the total comes to $#{@total}."
+  end
+
+  def void_last_transaction
+    return nil if @last == {}
+    
+    @total -= @last[:price] * @last[:quantity]
+    @last[:quantity].times { @items.pop }
+    @last = {}
+  end
+
+end
